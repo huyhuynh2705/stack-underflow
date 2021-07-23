222595ded0c9bf7fd14a53926ee34c2b897f4a0f
huyhuynh2705 <67199597+huyhuynh2705@users.noreply.github.com>
2021-07-23 23:58:21 +0700

    Deploy project

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Typography } from '@material-ui/core';
 
 const NotFound = () => (
-	<div>
-		<p>Click to route to "/oops" which isn't a registered route:</p>
-		<Link to='/oops'>Let's go</Link>
+	<div style={{ marginTop: '20px' }}>
+		<Typography variant='h4' align='center'>
+			Not Found
+		</Typography>
+		<Typography variant='h6' align='center'>
+			<Link to='/' style={{ textDecoration: 'none' }}>
+				Back to homepage
+			</Link>
+		</Typography>
 	</div>
 );
 
